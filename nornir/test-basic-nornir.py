80d35c5a309d6e6ddec00dd226d0eb43efc63b5f
moralest <moralest@lmax.com>
2020-07-03 13:59:56 +0100

    adding basic nornir test script

---
+++ nornir/test-basic-nornir.py
@@ -0,0 +1,17 @@
+from nornir import InitNornir
+
+def my_task(task):
+    print(task.host.hostname)
+    return f"This is a nornir task exec in {task.host.hostname}"
+
+def main():
+    #nr = InitNornir(core={"num_workers": 1})
+    nr = InitNornir(config_file="./config.yaml")
+    #nr = InitNornir()
+    aggresult = nr.run(task=my_task)
+    multiresult = aggresult['r1']
+    r = multiresult[0]
+    #ipdb.set_trace()
+
+if __name__ == "__main__":
+    main()
